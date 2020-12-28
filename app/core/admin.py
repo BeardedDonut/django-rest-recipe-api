64a957974f3f607cbb42308af1f8670b5ba5f456
Navid Ali Pour <navid9675@gmail.com>
2020-12-28 18:13:32 +0330

    Implement customize admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,28 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']                   # Order in the display by ID
+    list_display = ['email', 'name']    # display email and name
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name', )}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important Dates'), {'fields': ('last_login', )})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
