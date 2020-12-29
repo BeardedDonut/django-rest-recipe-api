259536d617e9a967619b29b9666313191db9fc35
Navid Ali Pour <navid9675@gmail.com>
2020-12-30 00:44:01 +0330

    Add user management endpoint for create User
    - Implement `User` serializer
    - Add `User` create url
    - Add `users` app to the settings
    - Implement `User` create view API using `generics.CreateAPIView`

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user"""
+    serializer_class = UserSerializer
