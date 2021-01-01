27f80892a10ebcc7f56554501c325761c100ed19
Navid Ali Pour <navid9675@gmail.com>
2021-01-01 19:27:02 +0330

    Add user authentication and token creation API

--- app/users/views.py
+++ app/users/views.py
@@ -1,8 +1,15 @@
 from rest_framework import generics
-
-from users.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from users.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
