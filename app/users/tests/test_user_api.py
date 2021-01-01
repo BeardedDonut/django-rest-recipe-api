4b88931a79f3164f4de9d74c96b33e81666f03dc
Navid Ali Pour <navid9675@gmail.com>
2021-01-01 18:41:08 +0330

    Add tests for create token API

--- app/users/tests/test_user_api.py
+++ app/users/tests/test_user_api.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('users:create')
+TOKEN_URL = reverse('users:token')
 
 
 def create_user(**params):
@@ -64,3 +65,54 @@ class PublicUserAPITests(TestCase):
             email=params['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that a token is created for users"""
+        params = {
+            'email': 'test@random.com',
+            'password': 'testpass',
+        }
+        create_user(**params)
+        response = self.client.post(TOKEN_URL, params)
+
+        self.assertIn('token', response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_token_for_invalid_credentials(self):
+        """Test that token not created when invalid credentials are passed"""
+        right_params = {
+            'email': 'test@random.com',
+            'password': 'right_password',
+        }
+        create_user(**right_params)
+
+        wrong_params = {
+            'email': 'test@random.com',
+            'password': 'wrong_password',
+        }
+        response = self.client.post(TOKEN_URL, wrong_params)
+
+        self.assertNotIn('token', response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test token not created when user is does not exist"""
+        params = {
+            'email': 'test@random.com',
+            'password': 'some_random_password',
+        }
+        response = self.client.post(TOKEN_URL, params)
+
+        self.assertNotIn('token', response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """Test that email and password are included in the params"""
+        params = {
+            'email': 'test@random.com',
+            'password': ''
+        }
+        response = self.client.post(TOKEN_URL, params)
+
+        self.assertNotIn('token', response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
