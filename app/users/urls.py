2997b1070709522a78ba108f00b0546802c22995
Navid Ali Pour <navid9675@gmail.com>
2021-01-02 21:35:26 +0330

    Implement user management private APIs

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,4 +6,6 @@ app_name = 'users'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('self/', views.ManageUserView.as_view(), name='self'),
+
 ]
