22af97693d7d06ee7b34def44441e56895aa031b
Navid Ali Pour <navid9675@gmail.com>
2021-01-16 22:14:01 +0330

    Implement Recipe listing API

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -41,3 +41,15 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def perform_create(self, serilizer):
         """Create a new Ingredient"""
         serilizer.save(user=self.request.user)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
